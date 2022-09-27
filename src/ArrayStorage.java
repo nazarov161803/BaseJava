f5f63d8fa438742c1a703802046afc5c1713905d
nazar <n.nazarov@cloudaware.com>
2022-09-27 11:05:27 +0400

    HW1 - added logic for crud methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,49 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i <= size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[size] = resume;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i =0; i<size; i++) {
+            if (storage[i].uuid.equalsIgnoreCase(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i <= size; i++) {
+            if (storage[i].uuid.equalsIgnoreCase(uuid)) {
+                storage[i] = null;
+                size--;
+            }
+            storage[i] = storage[i + 1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[size];
+        System.arraycopy(storage, 0, result, 0, size);
+        return result;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
